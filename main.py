cc44e46b9eca11ec05e48e62743a4ebde7bca168
Ben <ben.fattorri@mail.utoronto.ca>
2022-01-27 17:14:30 -0500

    Adding numpy data utils + conf

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import flax.linen as nn
 import optax
 from flax.training import train_state
+
+
+# Hydra 
+import hydra
+from hydra.utils import get_original_cwd
+from omegaconf import DictConfig, OmegaConf
+
+
+

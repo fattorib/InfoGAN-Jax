ea9731a46453730f1e97bde5314e02b64a451519
Ben <ben.fattorri@mail.utoronto.ca>
2022-01-29 18:34:17 -0500

    Updating latent creation, splitting random state

--- utils/create_generator_grid.py
+++ utils/create_generator_grid.py
@@ -2,16 +2,23 @@ from models.generator import *
 import matplotlib.pyplot as plt
 import jax
 import jax.numpy as jnp
-from utils.create_latents_with_codes import create_latents_with_codes
+from utils.create_latents_with_codes import create_latents_with_codes, create_latents_manual_categorical, create_latents_manual_cts
 
 
-def create_latent_grid(num_images, state, params, rng_key, image_dims=(10, 10)):
+def create_latent_grid(num_images, state, params, rng_key, image_dims=(10, 10), categorical_idx = None, cts_idx = None):
 
-    # TODO: Ability to vary codes
+    if categorical_idx is None and cts_idx is None:
+        latent_var = create_latents_with_codes(
+            num_noise=62, num_cts=2, num_cat=10, rng_key=rng_key, num_samples=num_images
+        )
 
-    latent_var = create_latents_with_codes(
-        num_noise=62, num_cts=2, num_cat=10, rng_key=rng_key, num_samples=num_images
-    )
+    elif categorical_idx is not None:
+        assert cts_idx is None
+        latent_var = create_latents_manual_categorical(num_noise=62, num_cts=2, num_cat=10, rng_key = rng_key, num_samples = 100)
+
+    elif cts_idx is not None:
+        assert categorical_idx is None
+        latent_var = create_latents_manual_cts(num_noise=62, num_cts=2, num_cat=10, rng_key = rng_key, cts_idx = cts_idx, num_samples = 100)
 
     output = state.apply_fn(
         {"params": params, "batch_stats": state.batch_stats},
@@ -44,3 +51,6 @@ def create_latent_grid(num_images, state, params, rng_key, image_dims=(10, 10)):
         ax.set_yticks([])
 
     return fig
+
+
+
--- utils/create_latents_with_codes.py
+++ utils/create_latents_with_codes.py
@@ -20,7 +20,10 @@ def create_latents_with_codes(num_noise, num_cts, num_cat, rng_key, num_samples)
 
     # Continuous code
     for _ in range(num_cts):
-        c = jax.random.uniform(key=rng_key, shape=(num_samples, 1), minval=-1, maxval=1)
+        #Need to split rng keys for each continuous code
+        rng_key, subkey = jax.random.split(rng_key)
+
+        c = jax.random.uniform(key=subkey, shape=(num_samples, 1), minval=-1, maxval=1)
         z = jnp.concatenate([z, c], axis=1)
 
     # Categorical code
@@ -34,3 +37,58 @@ def create_latents_with_codes(num_noise, num_cts, num_cat, rng_key, num_samples)
     z = jnp.concatenate([z, c], axis=1)
 
     return z
+
+
+
+def create_latents_manual_categorical(num_noise, num_cts, num_cat, rng_key, num_samples = 100):
+    """Create a latent variable to feed into the generator. 
+
+    Args:
+        
+    """
+
+    # Noise
+    z = jax.random.normal(rng_key, shape=(num_samples, num_noise))
+
+    # Continuous code
+    for _ in range(num_cts):
+        rng_key, subkey = jax.random.split(rng_key)
+
+        # Per paper, keep cts codes fixed
+        c = jnp.tile(jax.random.uniform(key=subkey, shape=(1, 1), minval=-1, maxval=1), num_samples).reshape(num_samples, 1)
+        z = jnp.concatenate([z, c], axis=1)
+
+
+    c = jnp.array([jnp.tile(jax.nn.one_hot(i, num_classes=num_cat), num_cat) for i in range(0,num_cat)]).reshape(num_samples,num_cat)
+
+    z = jnp.concatenate([z, c], axis=1)
+
+    return z
+
+
+def create_latents_manual_cts(num_noise, num_cts, num_cat, rng_key, cts_idx, num_samples = 100):
+    """Create a latent variable to feed into the generator. 
+
+    Args:
+        
+    """
+
+    # Noise
+    z = jax.random.normal(rng_key, shape=(num_samples, num_noise))
+
+    # Continuous code
+    for idx in range(num_cts):
+        if idx == cts_idx:
+            c = jnp.array(jnp.tile(jnp.linspace(-2,2,10), num_cat)).reshape(num_samples,1)
+            z = jnp.concatenate([z, c], axis=1)
+            
+        else:
+            rng_key, subkey = jax.random.split(rng_key)
+            c = jnp.tile(jax.random.uniform(key=subkey , shape=(1, 1), minval=-1, maxval=1),num_samples).reshape(num_samples,1)
+            z = jnp.concatenate([z, c], axis=1)
+    c = jnp.array([jnp.tile(jax.nn.one_hot(i, num_classes=num_cat), num_cat) for i in range(0,num_cat)]).reshape(num_samples,num_cat)
+
+    z = jnp.concatenate([z, c], axis=1)
+
+    return z
+
